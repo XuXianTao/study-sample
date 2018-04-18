f636d917aa126d2cecfac33cee9cb23ee682c189
XXT <ku8834367@163.com>
2018-04-19 00:06:15 +0800

    socket-io聊天室update0.0.1
    1. 增加聊天人数显示
    2. 增加成员退出提示
    3. 改进鼠标悬浮消息上方时候的日期显示

--- socket-io/public/chat.js
+++ socket-io/public/chat.js
@@ -1,21 +1,24 @@
 window.onload=function(){
 	var socket=io.connect();
 	socket.on('connect',function(){
-		socket.emit('join',prompt('What is your nickname?'));
+		socket.emit('join',prompt('What is your nickname?'),function(data) {
+			document.getElementById('people-counts').innerHTML=data;
+		});
 		document.getElementById('chat').style.display='block';
 	});
-	socket.on('announcement',function(msg){
+	socket.on('announcement',function(data){
 		var li=document.createElement('li');
 		li.className='announcement';
-		li.innerHTML=msg;
+		li.innerHTML=data.msg;
 		document.getElementById('messages').appendChild(li);
+		document.getElementById('people-counts').innerHTML=data.num;
 	});
 	var input=document.getElementById('input');
 	document.getElementById('form').onsubmit=function(){
 		var li=addMessage('me',input.value);
-		socket.emit('text',input.value,function (data){//回调函数的使用
+		socket.emit('text',input.value,function (date){//回调函数的使用
 			li.className='confirmed';
-			li.title=data;
+			li.title=date;
 		});
 		input.value='';
 		input.focus();
@@ -29,4 +32,7 @@ window.onload=function(){
 		document.getElementById('messages').appendChild(li);
 		return li;
 	}
+	socket.on('disconnect',function() {
+		socket.emit('disconnect');
+	})
 }
--- socket-io/server.js
+++ socket-io/server.js
@@ -14,13 +14,27 @@ var app=express()
 	.listen(3000);
 var io=sio.listen(app,{log:true});
 io.sockets.on('connection',function(socket){
+	var sname="";
 	console.log('Someone connected');
-	socket.on('join',function (name){
+	socket.on('join',function (name,callback){
+		sname=name;
 		socket.nickname=name;//那个发起连接的单独的客户端
-		socket.broadcast.emit('announcement',name+' joined the chat.');
+		let num = Object.keys(io.sockets.sockets).length;
+		socket.broadcast.emit('announcement',{msg:name+'&nbsp;joined the chat.',num:num});
+		console.log('%s joined the chat',sname);
+		console.log('the numbers:'+num);
+		callback(num);
 	});
 	socket.on('text',function(msg,callback){
 		socket.broadcast.emit('text',socket.nickname,msg);
-		callback(Date.now());
+		var date=new Date();
+		callback(date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate()+"\n"
+			+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds());
+	});
+	socket.on('disconnect', function(){
+		let num = Object.keys(io.sockets.sockets).length;
+		socket.broadcast.emit('announcement',{msg:socket.nickname+'&nbsp;leave the room.',num:num});
+	  console.log('%s leaved the chat',sname);
+	  console.log('the numbers:'+num);
 	});
 });
